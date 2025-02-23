6fdbfb0825538613d64cb5026a12fb16988f9756
Chris Peterson <cdpeters1@gmail.com>
2025-02-22 21:26:42 -0800

    Add enum definitions and schema overrides to utilities.py

--- notebooks/utilities.py
+++ notebooks/utilities.py
@@ -2,7 +2,27 @@ from pathlib import Path
 
 import marimo as mo
 from marimo._output.md import _md
+import polars as pl
 
+class_enum = pl.Enum(["FR", "SO", "JR", "SR"])
+# This refers to either offense, defense, or special teams.
+team_enum = pl.Enum(["OFF", "DEF", "ST"])
+dev_trait_enum = pl.Enum(["normal", "impact", "star", "elite"])
+
+# Set up the schema overrides for the columns that need it. The `overall_start`
+# and `overall_end` columns need an override because `polars` does not infer
+# the column as an integer for roster years that have this data missing.
+schema_overrides = {
+    "class": class_enum,
+    "position": pl.Categorical,
+    "group": pl.Categorical,
+    "secondary_group": pl.Categorical,
+    "team": team_enum,
+    "archetype": pl.Categorical,
+    "dev_trait": dev_trait_enum,
+    "overall_start": pl.UInt8,
+    "overall_end": pl.UInt8,
+}
 
 def find_project_path(project_name: str) -> Path:
     marker_files = [".git", "pyproject.toml"]
